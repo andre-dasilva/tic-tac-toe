e05817c67a65e74fb3117e7da714406963141ac7
André da Silva <andre-da.silva@outlook.com>
2020-01-04 16:30:05 +0100

    Add tic tac toe dotnet project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tic_tac_toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
